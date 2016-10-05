e0ace80dc40168d8e52d70891f27592d8a59b2d1
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-10-05 11:43:53 -0400

    CRUD is working

---
+++ app.rb
@@ -0,0 +1,8 @@
+require "sinatra"
+require "sinatra/reloader"
+require "active_record"
+
+require_relative "db/connection"
+require_relative "models/pokemon"
+require_relative "models/team"
+require_relative "models/trainer"
---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS pokemons;
+
+
+CREATE TABLE pokemons(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  poke_type VARCHAR,
+  cp INTEGER,
+  img_url TEXT
+);
