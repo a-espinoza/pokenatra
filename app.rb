d2fcb60e1e35c7db4286b18e1330d01fb7911b6b
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-10-05 15:25:30 -0400

    New working

--- app.rb
+++ app.rb
@@ -7,3 +7,22 @@ require_relative "db/connection"
 require_relative "models/pokemon"
 require_relative "models/trainer"
 require_relative "models/team"
+
+get '/pokemon' do
+  @pokemon = Pokemon.all
+  erb :"pokemon/index"
+end
+
+get '/pokemon/new' do
+  erb :"pokemon/new"
+end
+
+post '/pokemon' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemon/#{@pokemon.id}"
+end
+
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemon/show"
+end
